94e44ded9add6806d4507c555a87c447ffe018b7
Lucas S C. Jesus <lucas_55i@outlook.com>
2024-05-13 22:59:16 -0300

    ec2

---
+++ terraform/descomplicaTerraform/main.tf
@@ -0,0 +1,26 @@
+// Providers
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+variable "aws_region" {
+  type        = string
+  description = "The region in which the resources will be created"
+  default     = "us-east-1"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "descomplica-terraform-lucasjs-1"
+    key    = "terraform-test.tfstate"
+    region = "us-east-1"
+  }
+}
