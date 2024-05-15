9907ff2ff0fe77da1c641c4342185e851bb834a7
Lucas S C. Jesus <lucas_55i@outlook.com>
2024-05-14 22:54:47 -0300

    providers

--- terraform/descomplicaTerraform/ec2.tf
+++ terraform/descomplicaTerraform/ec2.tf
@@ -17,4 +17,26 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
- 
+
+data "aws_ami" "ubuntu_west" {
+  provider    = aws.west
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu 
+}
+
+resource "aws_instance" "web_west" {
+  provider      = aws.west
+  ami           = data.aws_ami.ubuntu_west.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
