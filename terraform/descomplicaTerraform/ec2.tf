3d2fa9e2bd6ee8bfde4039594626aafbdc1a4e67
Lucas S C. Jesus <lucas_55i@outlook.com>
2024-05-15 22:24:46 -0300

    variavies

--- terraform/descomplicaTerraform/ec2.tf
+++ terraform/descomplicaTerraform/ec2.tf
@@ -18,25 +18,25 @@ resource "aws_instance" "web" {
   }
 }
 
-data "aws_ami" "ubuntu_west" {
-  provider    = aws.west
-  most_recent = true
+# data "aws_ami" "ubuntu_west" {
+#   provider    = aws.west
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  owners = ["099720109477"] # Ubuntu 
-}
+#   owners = ["099720109477"] # Ubuntu 
+# }
 
-resource "aws_instance" "web_west" {
-  provider      = aws.west
-  ami           = data.aws_ami.ubuntu_west.id
-  instance_type = "t2.micro"
+# resource "aws_instance" "web_west" {
+#   provider      = aws.west
+#   ami           = data.aws_ami.ubuntu_west.id
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
 
---
+++ terraform/descomplicaTerraform/variables.auto..tfvars
@@ -0,0 +1,5 @@
+variable "image_id" {
+  type        = string
+  description = "the id of the machie image(AMI) to use for the server."
+  default     = "ami-123345678"
+}
