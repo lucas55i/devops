d0a250abe7c01441f4e8ed6f8d87195a5554390a
Lucas Jesus <lucas.devj@outlook.com>
2025-06-17 22:14:17 -0300

    feat: decouple tags, cidrsubnet function:

--- Iac/terraform/udemyCurso/public.tf
+++ Iac/terraform/udemyCurso/public.tf
@@ -3,12 +3,13 @@ resource "aws_subnet" "eks_subnet_puclic_1a" {
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name                     = "comunidadedevops-subnet-1a",
-    "kubernetes.io/role/elb" = 1
-
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-1a",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
 }
 
 resource "aws_subnet" "eks_subnet_puclic_1b" {
@@ -16,9 +17,11 @@ resource "aws_subnet" "eks_subnet_puclic_1b" {
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name                     = "comunidadedevops-subnet-1b",
-    "kubernetes.io/role/elb" = 1
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-1b",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
 }
