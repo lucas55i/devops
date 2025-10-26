a8a3ac3b15ba8e923a9da196e21078a7646ffd03
lucas55i <lucas55i@outlook.com>
2025-10-26 18:11:38 -0300

    loops + ex02

---
+++ Automation/script/shell/mod1/ex02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+for i in $(seq 0 10)
+do
+ [ $(($i % 2)) -eq 0 ] && echo "Número $i é divisível por 2"
+done
--- Automation/script/shell/mod1/loop.sh
+++ Automation/script/shell/mod1/loop.sh
@@ -10,3 +10,23 @@ echo "FOR 2 ======"
 for i in $(seq 10); do
   echo $i
 done
+
+
+echo "==== For 3 (array)"
+Frutas=(
+'Laranja'
+'Ameixa'
+'Abacaxi'
+)
+
+for i in "${Frutas[@]}"; do
+  echo $i
+done
+
+
+echo "===== While"
+contador=0
+while [ $contador -lt ${#Frutas[@]} ]; do
+  echo $contador
+  contador=$(($contador+1))
+done
